c44ba12adb8b232733a33510d59365a0a0177a51
GC <otoku118@aol.com>
2022-03-09 19:48:19 +0300

    updated the package json

--- contracts/SolidityTest.sol
+++ contracts/SolidityTest.sol
@@ -1,27 +1,42 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract SolidityTest {
-    uint public constant PRICE = 1 ether / 10;
-    uint public constant MAX_SUPPLY = 1000;
-    uint public constant MAX_PER_TX = 5;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@1001-digital/erc721-extensions/contracts/LinearlyAssigned.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
-    // YOUR CODE HERE
-    // ---
+contract SolidityTest is Ownable, ERC721, LinearlyAssigned {
+    uint256 public constant PRICE = 1 ether / 10;
+    uint256 public constant MAX_SUPPLY = 1000;
+    uint256 public constant MAX_PER_TX = 5;
 
-    constructor() {
-        // YOUR CODE HERE
+    modifier onlyUnderLimitPerTx(uint256 amount) {
+        require(amount <= MAX_PER_TX, "can't mint more than limit per tx");
+        _;
+    }
+
+    constructor() ERC721("Test", "TST") LinearlyAssigned(MAX_SUPPLY, 1) {
     }
 
     function claim() external {
-        // YOUR CODE HERE
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success);
     }
 
-    function mint(uint256 amount) external payable {
-        // YOUR CODE HERE
+    function mint(uint256 amount)
+        external
+        payable
+        ensureAvailabilityFor(amount)
+        onlyUnderLimitPerTx(amount)
+    {
+        require(PRICE * amount == msg.value, "insufficient fund");
+        for (uint256 index = 0; index < amount; index++) {
+            _safeMint(msg.sender, nextToken());
+        }
     }
 
-    function ownerOf(uint256 tokenId) external view returns (address) {
-        // YOUR CODE HERE
+    function ownerOf(uint256 tokenId) public view override returns (address) {
+        return super.ownerOf(tokenId);
     }
 }
